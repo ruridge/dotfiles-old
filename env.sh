4489d22f60791d9bc9dc46b1cb499ef373ad324f
Rob Uridge <robu@brandwatch.com>
2017-08-02 10:46:21 +0100

    add nocors canary and tmux mouse conf

--- env.sh
+++ env.sh
@@ -11,6 +11,9 @@ function r() { grep "$1" ${@:2} -R . }
 #mkdir and cd
 function mkcd() { mkdir -p "$@" && cd "$_"; }
 
-# Use sublimetext for editing config files
+# Use vim for editing config files
 alias zshconfig="vim ~/.zshrc"
 alias envconfig="vim ~/Code/config/env.sh"
+
+# Open Chrom Canary with CORS disabled
+function nocors() { open -a Google\ Chrome\ Canary --args --disable-web-security --user-data-dir }
