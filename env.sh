bdc405cb7d38b1bb2dfd0f4bde79584b3a47e74d
Rob Uridge <ruridge@gmail.com>
2019-09-22 21:44:34 +0100

    add tmux

--- env.sh
+++ env.sh
@@ -14,6 +14,20 @@ function mkcd() { mkdir -p "$@" && cd "$_"; }
 # Use vim for editing config files
 alias zshconfig="vim ~/.zshrc"
 alias envconfig="vim ~/Code/config/env.sh"
+alias tmuxconfig="vim ~/.tmux.conf"
 
-# Open Chrom Canary with CORS disabled
+# tmux aliases
+
+# Attaches tmux to the last session; creates a new session if none exists.
+alias t="tmux attach || tmux new-session"
+# Attaches tmux to a session (example: ta front)
+alias ta="tmux attach -t"
+# Creates a new session (example: tn front)
+alias tn="tmux new-session -s"
+# Lists all ongoing sessions
+alias tl="tmux list-sessions"
+# Kills a tmux session
+alias tk="tmux kill-session -t"
+
+# Open Chrome Canary with CORS disabled
 function nocors() { open -a Google\ Chrome\ Canary --args --disable-web-security --user-data-dir }
